1fc34e5fe4b71521f84f0bbaefcee05bf2b6e0fa
Eca Moleong <ezra.moleong@gmail.com>
2020-06-08 16:06:44 +0800

    tambah folder pertemuan 3

---
+++ pertemuan3/ByteShortIntLong/src/com/multiplatform/Main.java
@@ -0,0 +1,16 @@
+package com.multiplatform;
+
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+        int myMaxIntValue = Integer.MAX_VALUE;
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer Maximum value = " + myMaxIntValue);
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Overflow Max value = " + (myMaxIntValue + 1));
+        System.out.println("Underflow Min value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+    }
+}
