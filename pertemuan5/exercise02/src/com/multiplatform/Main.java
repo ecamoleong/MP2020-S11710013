aeebea4dec5c4ab4001a57e07df3ed63391b6af0
Eca Moleong <ezra.moleong@gmail.com>
2020-06-10 15:55:20 +0800

    exercise 02

---
+++ pertemuan5/exercise02/src/com/multiplatform/Main.java
@@ -0,0 +1,11 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double myDoubleValue = 50;
+        double converted = myDoubleValue * 0.45359237d;
+
+        System.out.println("myDoubleValue = " + myDoubleValue);
+    }
+}
