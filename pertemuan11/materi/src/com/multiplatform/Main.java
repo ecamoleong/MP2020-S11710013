e4826dfee577cee6694fe58ed13ab2fe886687db
Eca Moleong <ezra.moleong@gmail.com>
2020-06-18 16:52:52 +0800

    exercise 6

---
+++ pertemuan11/materi/src/com/multiplatform/Main.java
@@ -0,0 +1,11 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Class & Object
+        car toyota = new car();
+        toyota.setModel("Agya");
+        System.out.println("Model is " + toyota.getModel());
+    }
+}
---
+++ pertemuan11/materi/src/com/multiplatform/car.java
@@ -0,0 +1,21 @@
+package com.multiplatform;
+
+public class car {
+    //2 member dalam class
+    //1. field
+    //2. method
+
+    private String model;
+    private int doors;
+    private int wheels;
+    private String engine;
+
+    //Setter
+    public void setModel(String model) {
+        this.model = model;
+    }
+    //Getter
+    public String getModel(){
+        return this.model;
+    }
+}
